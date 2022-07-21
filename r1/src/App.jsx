41dd4e7367a5494751be9292b2152544cf226d99
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-21 14:30:51 +0300

    kv

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,17 +1,28 @@
 import './App.css';
-import Fun from './Components/006/Fun';
+// import Fun from './Components/006/Fun';
 import { useState } from "react";
-import Sad from './Components/006/Sad';
+import randColor from './Functions/randColor';
+// import Sad from './Components/006/Sad';
 function App() {
 
-    const [count, setCount] =useState(0);
+    // const [count, setCount] =useState(0);
+
+    const [kv, setKv] = useState([]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>State</h1>
-            <Fun spalva="pink" setCount={setCount}></Fun>
-            <Sad count={count}></Sad>
+        <h1 onClick={() => setKv(k => [...k, randColor()])}>State</h1>
+            {/* <Fun spalva="pink" setCount={setCount}></Fun>
+            <Sad count={count}></Sad> */}
+            <div className="kv-bin">
+                {
+                    kv.map((c, i) => <div className="kv" style={{
+                        backgroundColor: c+'69',
+                        borderColor: c
+                    }} key={i}></div>)
+                }
+            </div>
         </header>
     </div>
   );
