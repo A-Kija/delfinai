0d686fa5fa354cfd9fb66d4fe7198a3a1596bf98
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-21 13:20:44 +0300

    s

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Fun from './Components/006/Fun';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>State</h1>
-
+            <Fun spalva="pink"></Fun>
         </header>
     </div>
   );
