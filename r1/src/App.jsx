e006ee09e3b96f75eae9de0d31bf3f57d39dbe49
A-Kija <arvydas.kijakauskas@gmail.com>
2022-07-25 12:21:20 +0300

    jb

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,31 +1,23 @@
 import './App.css';
-// import Fun from './Components/006/Fun';
 import { useState } from "react";
 import randColor from './Functions/randColor';
-// import Sad from './Components/006/Sad';
+import Kv from './Components/JamesBond/Kv';
 function App() {
-
-    // const [count, setCount] =useState(0);
 
     const [kv, setKv] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 onClick={() => setKv(k => [...k, randColor()])}>State</h1>
-            {/* <Fun spalva="pink" setCount={setCount}></Fun>
-            <Sad count={count}></Sad> */}
-            <div className="kv-bin">
-                {
-                    kv.map((c, i) => <div className="kv" style={{
-                        backgroundColor: c+'69',
-                        borderColor: c
-                    }} key={i}></div>)
-                }
-            </div>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 onClick={() => setKv(k => [...k, randColor()])}>State</h1>
+                <div className="kv-bin">
+                    {
+                        kv.map((c, i) => <Kv key={i} c={c}></Kv>)
+                    }
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ r1/src/Components/JamesBond/Kv.jsx
@@ -0,0 +1,11 @@
+function Kv({c}) {
+
+    return(
+        <div className="kv" style={{
+            backgroundColor: c + '66',
+            borderColor: c
+        }}></div>
+    )
+}
+
+export default Kv;
