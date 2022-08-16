90143e3d45de9ed936c594ab057f5e160a32baec
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-16 14:35:15 +0300

    jap

--- 019/index.php
+++ 019/index.php
@@ -15,11 +15,19 @@ function router() {
 
     $url = explode('/', $url);
 
-    print_r($url);
+    // print_r($url);
 
 
     if ($url[0] == 'add-money') {
+        
         require __DIR__ . '/inc/add.php';
+    }
+    else if (count($url) == 2 && $url[0] == 'remove-money') {
+        require __DIR__ . '/inc/rem.php';
+    }
+
+    else {
+        echo '<h1>404</h1>';
     }
 
 
