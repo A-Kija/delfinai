d707b8221e7980afb838c22acd8ad77fdba513d6
A-Kija <arvydas.kijakauskas@gmail.com>
2022-08-22 14:05:24 +0300

    singleton

--- 022/Kibiras.php
+++ 022/Kibiras.php
@@ -4,11 +4,28 @@ class Kibiras {
 
     protected $akmenuKiekis;
     static private $akmenuKiekisVisuoseKibiruose = 0;
+    static private $manoKibiras;
 
+    static public function kiekYraAkmenu() : int
+    {
+        return self::$akmenuKiekisVisuoseKibiruose;
+    }
 
-    public function __construct()
+    static public function naujasKibiras() : Kibiras
+    {
+        return self::$manoKibiras ?? self::$manoKibiras = new self;
+    }
+
+    private function __construct()
     {
         $this->akmenuKiekis = 0;
+    }
+
+    private function __clone(){}
+
+    public function __wakeup()
+    {
+        throw new Exception('Šlovė Meškėnams!');
     }
 
     public function prideti1Akmeni() : void
