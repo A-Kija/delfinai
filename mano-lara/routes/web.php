c4eb0766ee23ebe70979d791724f0bedcbb09641
A-Kija <arvydas.kijakauskas@gmail.com>
2022-09-08 13:14:37 +0300

    taip

--- mano-lara/routes/web.php
+++ mano-lara/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NiceController as Nice;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/eziukas', fn() => '<h1>EŽIUKAS</h1>'); 
+Route::get('/bebriukas/super', function() { return '<h1>NE EŽIUKAS</h1>';}); 
+
+
+Route::get('/fun/{kiek}/{abc}', [Nice::class, 'fun']);
